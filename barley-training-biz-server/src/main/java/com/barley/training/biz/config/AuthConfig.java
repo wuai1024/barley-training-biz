0c28d822eb4cd40182f7227bdc6e7060ffda5dd6
Lix <lixs@billbear.cn>
2025-01-03 16:28:10 +0800

    [add] Auth

--- barley-training-biz-server/src/main/java/com/barley/training/biz/config/AuthConfig.java
+++ barley-training-biz-server/src/main/java/com/barley/training/biz/config/AuthConfig.java
@@ -1,25 +1,37 @@
 package com.barley.training.biz.config;
 
 import com.barley.common.auth.session.MyAuthSession;
+import com.barley.common.auth.session.SessionService;
+import com.barley.common.base.JsonUtils;
 import com.barley.common.base.LoginUser;
+import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
+import java.util.Objects;
+
 @Component
+@RequiredArgsConstructor
 public class AuthConfig implements MyAuthSession {
+    private final SessionService sessionService;
+
     @Override
     public boolean checkToken(String token) {
-        return true;
+        return sessionService.checkToken(token);
     }
 
     @Override
     public LoginUser getUserSession(String authorize, String value) {
-        final LoginUser a = new LoginUser();
-        a.setId("1");
-        return a;
+        final String userId = Objects.toString(sessionService.getToken(authorize), null);
+        if (StringUtils.isBlank(userId)) {
+            return null;
+        }
+        return sessionService.getUserSession(userId);
     }
 
     @Override
     public String failResponse(String message) {
-        return "";
+        return JsonUtils.stringify(Map.of("message", message, "code", "401"));
     }
 }
